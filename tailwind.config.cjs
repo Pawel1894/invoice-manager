300b510be5f1fd005b3f5d25695827089ab9aad5
Pawel1894 <pawepohl@gmail.com>
2023-03-17 21:13:29 +0100

    Project init

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/**/*.{jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
