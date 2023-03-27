c65b02fb75c57c39b7f667c66544660d8ac3f4b1
Pawel1894 <pawepohl@gmail.com>
2023-03-27 19:54:14 +0200

    invoice card dark mode

--- src/components/Invoice.tsx
+++ src/components/Invoice.tsx
@@ -9,9 +9,9 @@ const statusStyle: {
   [key in Status]: { bg: string; text: string; accent: string };
 } = {
   DRAFT: {
-    bg: "bg-neutral-600",
-    accent: "bg-neutral-400",
-    text: "text-neutral-400",
+    bg: "bg-neutral-600 dark:bg-neutral-200",
+    accent: "bg-neutral-400 dark:bg-neutral-300",
+    text: "text-neutral-400 dark:text-neutral-300",
   },
   PAID: {
     bg: "bg-accent-800",
@@ -36,27 +36,27 @@ export default function Invoice({
   return (
     <Link
       href={`/invoice/${id}`}
-      className="grid w-full grid-cols-2 gap-y-2 rounded-lg bg-white p-6 shadow-light"
+      className="grid w-full  grid-cols-2 gap-y-2 rounded-lg bg-white p-6 shadow-light dark:bg-neutral-100"
     >
-      <span className="row-start-1 mb-5 justify-self-start font-bold">
-        <span className="text-accent-300">#</span>
+      <span className="row-start-1 mb-5 justify-self-start font-bold  dark:text-white">
+        <span className="text-accent-300 ">#</span>
         {number}
       </span>
       <span className="col-span-1 row-start-2 justify-self-start text-sm text-accent-300">
         Due {dayjs(dueDate).format("DD MMM YYYY")}
       </span>
-      <span className="col-span-2 row-start-1 justify-self-end text-right text-sm text-accent-300">
+      <span className="col-span-2 row-start-1 justify-self-end text-right text-sm text-accent-300 dark:text-white">
         {clientName}
       </span>
       <span
         title={format(totalAmount, "en-GB")}
-        className="col-start-1 row-start-3 w-full justify-self-start overflow-hidden text-ellipsis whitespace-nowrap text-left"
+        className="col-start-1 row-start-3 w-full justify-self-start overflow-hidden text-ellipsis whitespace-nowrap text-left dark:text-white"
       >
         {format(totalAmount, "en-GB")}
       </span>
       <div className="row-span-2 flex items-center justify-self-end">
         <div
-          className={`rounded-md ${statusStyle[status].bg}  py-2 px-[1.6875rem]`}
+          className={`rounded-md ${statusStyle[status].bg}  py-2 px-[1.6875rem] `}
         >
           <span
             className={`mr-2 inline-block h-2 w-2 rounded-full  ${statusStyle[status].accent}`}
